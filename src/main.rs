adced213b22c5cbb6ac0034f287d97323791615c
rberga06 <78449715+RBerga06@users.noreply.github.com>
2025-07-02 21:56:25 +0200

    feat(main): define a pyo3 module that is importable by Python

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
-use pyo3::Python;
+#[pyo3::pymodule(name="pyo3_conda_test")]
+mod py {
+    use pyo3::{pyclass, pymethods};
+
+    #[pyclass]
+    /// A Rust-managed counter that cannot be decremented
+    #[derive(Clone, Debug)]
+    pub struct Counter { counter: usize }
+    #[pymethods]
+    impl Counter {
+        #[new]
+        #[pyo3(signature = (value = 0, /))]
+        pub fn new(value: usize) -> Self { Self { counter: value } }
+        #[pyo3(signature = (*, by = 1))]
+        fn increment(&mut self, by: usize) { self.counter += by }
+        #[getter]
+        fn value(&self) -> usize { self.counter }
+    }
+}
 
 fn main() {
-    Python::with_gil(|py| {
+    pyo3::append_to_inittab!(py);
+    pyo3::Python::with_gil(|py| {
         println!("Python: {}", py.version());
-    })
+        py.run(pyo3::ffi::c_str!(r#"
+from pyo3_conda_test import Counter
+c = Counter()
+assert c.value == 0
+c.increment()
+assert c.value == 1
+c.increment(by=42)
+assert c.value == 43
+help(Counter)
+import sys
+sys.stdout.flush()
+        "#), None, None)
+    }).unwrap()
 }
